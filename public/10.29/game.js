20ee2ea505537d4a6919e8599319e4f316f737d4
Reborn233 <499289113@qq.com>
2017-11-01 17:31:04 +0800

    push

--- public/10.29/game.js
+++ public/10.29/game.js
@@ -7,14 +7,13 @@ class Game {
         document.body.appendChild(canvas)
         canvas.width = width
         canvas.height = height
-        ctx.fillStyle = style || '#000'
+        this.color = style || '#000'
         this.preload = obj.preload
         this.start = obj.start
         this.keyBoard = keyPush()
         this.fps = 60
         this.scene = null
         this.images = {}
-        this.pause = false
         window.addEventListener('keydown', (e) => {
             this.keyBoard.keydowns[e.key] = true
         })
@@ -87,7 +86,7 @@ class Game {
     }
 
     drawRect(obj) {
-        ctx.fillStyle = obj.color || '#000'
+        ctx.fillStyle = obj.color || this.color
         ctx.fillRect(obj.x, obj.y, obj.width, obj.height)
     }
 
@@ -126,11 +125,22 @@ class Sprite {
 
     update() {
     }
+
+    setSpeed(speedX, speedY) {
+        this.speedX = speedX
+        this.speedY = speedY
+    }
+
+    setPosition(x, y) {
+        this.x = x
+        this.y = y
+    }
 }
 
 class Scene {
     constructor(game) {
         this.game = game
+        this.sprites = []
     }
 
     static new(game, ...args) {
@@ -139,22 +149,41 @@ class Scene {
     }
 
     draw() {
+        const g = this.game
+        let bg = {
+            x: 0,
+            y: 0,
+            width: canvas.width,
+            height: canvas.height,
+        }
+        g.drawRect(bg)
+        this.sprites.forEach((sprite) => {
+            if (sprite.image) {
+                g.drawImage(sprite)
+            } else {
+                g.drawRect(sprite)
+            }
+        })
     }
 
     update() {
+        const g = this.game
+        this.sprites.forEach((sprite) => {
+            sprite.update && sprite.update(this.game)
+        })
     }
-}
 
-class Background extends Sprite {
-    constructor(...args) {
-        super(...args)
+    addSprite(obj) {
+        obj.scene = this
+        this.sprites.push(obj)
     }
+
 }
 
 class Hp {
-    constructor(x, y, width, height, color) {
-        this.x = x
-        this.y = y
+    constructor(width, height, color) {
+        this.x = 0
+        this.y = 0
         this.width = width
         this.height = height
         this.color = color || '#000'
@@ -163,6 +192,9 @@ class Hp {
     static new(...args) {
         let i = new this(...args)
         return i
+    }
+
+    update(g) {
     }
 
     add(num) {
@@ -186,35 +218,86 @@ class Hp {
 class Player extends Sprite {
     constructor(...args) {
         super(...args)
+        this.setup && this.setup()
+    }
+
+    setup() {
+        this.x = 15
+        this.y = canvas.height - this.height
+        this.speedY = 0
+        this.speedX = 2
+        this.attr = {
+            hp: 50
+        }
+        this.equip = null
+        this.cooling = 0
+        this.isDown = true
+        this.direction = 'right'
+    }
+
+    update() {
+        let wq = this.direction === 'left' ? Player.new(id['wq_l.png']) : Player.new(id['wq.png'])
+        let weapon = this.direction === 'left' ? Player.new(id['mp5_l.png']) : Player.new(id['mp5.png'])
+        this.posX = wq.posX
+        this.posY = wq.posY
+        this.weapon.posX = weapon.posX
+        this.weapon.posY = weapon.posY
+        if (this.cooling > 0) {
+            this.cooling--
+        }
+        if (this.y >= canvas.height - this.height) {
+            this.speedY = 0
+            this.isDown = true
+        }
+        if (this.y <= canvas.height - this.height - 15) {
+            this.speedY += 1
+        }
+        this.y += this.speedY
+        this.hp1.setPosition(this.x + this.width / 2 - this.hp1.width / 2, this.y - 10)
+        this.hp2.setPosition(this.x + this.width / 2 - this.hp1.width / 2, this.y - 10)
+        if (this.direction === 'left') {
+            this.weapon.setPosition(this.x - this.weapon.width / 3, this.y + 35)
+        } else {
+            this.weapon.setPosition(this.x + this.width - this.weapon.width * 2 / 3, this.y + 35)
+        }
+    }
+
+    fire() {
+        if (this.cooling === 0) {
+            this.cooling = 9
+            let bullet = Bullet.new(id['bullet.png'])
+            bullet.direction = this.direction
+            bullet.x = this.direction === 'right' ? this.weapon.x + this.weapon.width : this.weapon.x
+            bullet.y = this.weapon.y - 1
+            this.scene.sprites.bullets.push(bullet)
+        }
     }
 
     move(direction) {
-        if (window.pause) {
-            return
-        }
+        this.direction = direction
         switch (direction) {
             case 'left':
-                if (this.x <= 32) {
-                    this.x = 32
+                if (this.x <= 0) {
+                    this.x = 0
                 } else {
                     this.x -= this.speedX
                 }
                 break
             case 'right':
-                if (this.x >= canvas.width - this.width - 32) {
-                    this.x = canvas.width - this.width - 32
+                if (this.x >= canvas.width * 2 / 3 - this.width) {
+                    this.x = canvas.width * 2 / 3 - this.width
                 } else {
                     this.x += this.speedX
                 }
                 break
-            case 'bottom':
+            case 'down':
                 if (this.y >= canvas.height - this.height - 32) {
                     this.y = canvas.height - this.height - 32
                 } else {
                     this.y += this.speedY
                 }
                 break
-            case 'top':
+            case 'up':
                 if (this.y <= 32) {
                     this.y = 32
                 } else {
@@ -224,223 +307,268 @@ class Player extends Sprite {
         }
     }
 
-    setSpeed(speedX, speedY) {
-        this.speedX = speedX
-        this.speedY = speedY
+    jump() {
+        this.isDown = false
+        this.speedY = -15
+        this.y += this.speedY
     }
 
-    setPosition(x, y) {
-        this.x = x
-        this.y = y
-    }
-}
-
-class Monster {
-    constructor(game) {
-        this.game = game
-        this.blobs = []
+    subHp() {
+        if (this.attr.hp <= 0) {
+            this.attr.hp = 0
+        } else {
+            this.attr.hp -= 1
+            this.hp2.sub()
+        }
     }
 
-    draw() {
-        const g = this.game
-        this.blobs.forEach((blob, index) => {
-            g.drawImage(blob)
-        })
+    addAttr(obj, name) {
+        this.attr[name] = obj
     }
 }
 
-class Bullet {
-    constructor(game) {
-        this.game = game
+class Bullet extends Sprite {
+    constructor(...args) {
+        super(...args)
+        this.setup()
+    }
+
+    setup() {
+        this.speedX = 7
+        this.speedY = 7
+        this.cooling = 0
+    }
+
+    update() {
+        if (this.direction === 'right') {
+            this.x += this.speedX
+        } else {
+            this.x -= this.speedX
+        }
+    }
+}
+
+class Weapon extends Sprite {
+    constructor(...args) {
+        super(...args)
+        this.setup()
+    }
+
+    setup() {
+        this.bulletNum = 200
+    }
+}
+
+class Monster extends Sprite {
+    constructor(...args) {
+        super(...args)
+        this.setup()
+    }
+
+    setup() {
+        this.speedX = .3
+        this.x = canvas.width - 200
+        this.y = canvas.height - this.height
+        this.attr = {
+            hp: 150
+        }
         this.bullets = []
+        this.cooling = 0
     }
 
-    draw() {
-        const g = this.game
-        this.bullets.forEach((bullet, index) => {
-            bullet.x += bullet.speedX
-            g.drawImage(bullet)
-        })
+    update() {
+        if (this.cooling > 0) {
+            this.cooling--
+        }
+        this.hp1.setPosition(this.x + this.width / 2 - this.hp1.width / 2, this.y - 40)
+        this.hp2.setPosition(this.x + this.width / 2 - this.hp1.width / 2, this.y - 40)
+        // this.x -= this.speedX
+        // if (this.x <= 0 - this.width) {
+        //     this.setup()
+        // }
+        this.spread()
+
+    }
+
+    spread() {
+        for (let i = 0; i < randomNum(30, 50); i++) {
+            let bullet2 = Bullet.new(id['bullet2.png'])
+            bullet2.x = this.x + this.width / 2 - bullet2.width / 2
+            bullet2.y = this.y + this.height / 2 - bullet2.height / 2
+            bullet2.speedX = randomNum(-5, 5)
+            bullet2.speedY = randomNum(-5, 5)
+            bullet2.update = () => {
+                bullet2.x += bullet2.speedX
+                bullet2.y += bullet2.speedY
+            }
+            if (this.cooling === 0) {
+                this.cooling = 4
+                this.bullets.push(bullet2)
+            }
+        }
+    }
+
+    subHp() {
+        if (this.attr.hp <= 0) {
+            this.attr.hp = 0
+        } else {
+            this.attr.hp -= 1
+            this.hp2.sub()
+        }
     }
 }
 
 class SceneStart extends Scene {
     constructor(game) {
         super(game)
-        this.text = {}
-        this.create()
-        game.registerAction('1', () => {
-            scene2 = SceneMain.new(game)
-            game.replaceScene(scene2)
+        this.title = {
+            txt: '按 1 开始',
+            x: 120,
+            y: 120,
+            color: '#000',
+        }
+        this.setup(game)
+    }
+
+    setup(g) {
+        g.registerAction('1', () => {
+            let s = SceneMain.new(g)
+            g.replaceScene(s)
         })
     }
 
     draw() {
         const g = this.game
-        for (let key in this.text) {
-            g.drawText(this.text[key])
-        }
+        g.drawText(this.title)
     }
 
-    create() {
-        let title = {
-            name: 'title',
-            txt: '按 1 开始',
-            x: 120,
-            y: 120
-        }
-        this.addText(title)
-    }
-
-
-    addText(obj) {
-        this.text[obj.name] = obj
+    update() {
     }
 }
 
 class SceneMain extends Scene {
     constructor(game) {
         super(game)
-        this.sprites = {}
-        this.rects = {}
-        this.list = {}
-        this.create()
+        this.setup()
+    }
 
-        this.keyPush()
-        window.addEventListener('keyup', (e) => {
-            switch (e.key) {
-                case 'j':
-                    this.sprites['bullet'].fire()
-                    break
+    setup() {
+        const g = this.game
+        this.sprites.bullets = []
+        //玩家
+        this.wq = Player.new(id['wq.png'])
+        let p = this.wq
+        //玩家血条
+        p.hp1 = Hp.new(p.attr.hp, 5)
+        p.hp2 = Hp.new(p.attr.hp, 5, '#E13B3D')
+        //武器
+        p.weapon = Weapon.new(id['mp5.png'])
+
+        //blob
+        this.yt = Monster.new(id['yt.png'])
+        let b = this.yt
+        //blob血条
+        b.hp1 = Hp.new(b.attr.hp, 10)
+        b.hp2 = Hp.new(b.attr.hp, 10, '#E13B3D')
+
+        this.addSprite(b)
+        this.addSprite(b.hp1)
+        this.addSprite(b.hp2)
+        this.addSprite(p)
+        this.addSprite(p.hp1)
+        this.addSprite(p.hp2)
+        this.addSprite(p.weapon)
+
+
+        g.registerAction('j', () => {
+            p.fire()
+        })
+        g.registerAction('a', () => {
+            p.move('left')
+        })
+        g.registerAction('w', () => {
+            if (p.isDown) {
+                p.jump()
             }
+        })
+        g.registerAction('d', () => {
+            p.move('right')
+        })
+    }
+
+    draw() {
+        super.draw()
+        let bullet = this.sprites.bullets
+        bullet.forEach((b, i) => {
+            this.game.drawImage(b)
+        })
+        this.yt.bullets.forEach((b2, i) => {
+            this.game.drawImage(b2)
+        })
+
+    }
+
+    update() {
+        super.update()
+        if(this.wq.attr.hp<=0){
+            alert('你死了')
+            let s = SceneEnd.new(this.game)
+            this.game.replaceScene(s)
+            return
+        }
+        if(this.yt.attr.hp<=0){
+            alert('你赢了')
+            let s = SceneEnd.new(this.game)
+            this.game.replaceScene(s)
+            return
+        }
+        this.sprites.bullets.forEach((b, i) => {
+            b.update()
+            if (b.x >= canvas.width - b.width) {
+                this.sprites.bullets.splice(i, 1)
+            }
+            if (isCollide(b, this.yt)) {
+                this.yt.x += 5
+                this.yt.subHp()
+                this.sprites.bullets.splice(i, 1)
+            }
+        })
+        this.yt.bullets.forEach((b2, i) => {
+            b2.update()
+            if (b2.x <= 0 || b2.x >= canvas.width || b2.y <= 0 || b2.y >= canvas.height) {
+                this.yt.bullets.splice(i,1)
+            }
+            if(isCollide(b2,this.wq)){
+                this.wq.subHp()
+            }
+        })
+    }
+}
+
+class SceneEnd extends Scene {
+    constructor(game) {
+        super(game)
+        this.title = {
+            txt: '游戏结束 按 2 重来',
+            x: 120,
+            y: 120,
+            color: '#000',
+        }
+        this.setup(game)
+    }
+
+    setup(g) {
+        g.registerAction('2', () => {
+            let s = SceneStart.new(g)
+            g.replaceScene(s)
         })
     }
 
     draw() {
         const g = this.game
-        const s = this.sprites
-        const r = this.rects
-        const l = this.list
-        r['phpout'].setPosition(s['wq'].x + s['wq'].width / 2 - r['phpout'].width / 2, s['wq'].y - 10)
-        r['phpin'].setPosition(s['wq'].x + s['wq'].width / 2 - r['phpout'].width / 2, s['wq'].y - 10)
-        s['mp5'].setPosition(s['wq'].x + s['wq'].width - 22, s['wq'].y + s['wq'].height - 14)
-        Object.keys(this.sprites).forEach((key) => {
-            g.drawImage(this.sprites[key])
-        })
-        Object.keys(this.rects).forEach((key) => {
-            g.drawRect(this.rects[key])
-        })
-        s['bullet'].bullets.forEach((bullet, index) => {
-            if (index > 10) {
-                return
-            }
-            bullet.x += s['bullet'].speedX
-            s['bullet'].x = bullet.x
-            s['bullet'].y = bullet.y
-            g.drawImage(s['bullet'])
-            this.blobs.forEach((blob, j) => {
-                log(blob)
-                if (isCrash(bullet, blob)) {
-                    log('击中')
-                    s['bullet'].bullets.splice(index, 1)
-                }
-            })
-
-            if (s['bullet'].x >= canvas.width - s['bullet'].width - 32) {
-                s['bullet'].bullets.splice(index, 1)
-            }
-        })
-        l['monster'].draw()
+        g.drawText(this.title)
     }
 
     update() {
-    }
-
-    keyPush() {
-        const g = this.game
-        const s = this.sprites
-        const r = this.rects
-        g.registerAction('j', () => {
-        })
-        g.registerAction('a', () => {
-            s['wq'].move('left')
-        })
-        g.registerAction('w', () => {
-            s['wq'].move('top')
-        })
-        g.registerAction('d', () => {
-            s['wq'].move('right')
-        })
-        g.registerAction('s', () => {
-            s['wq'].move('bottom')
-        })
-    }
-
-    create() {
-        const g = this.game
-        //背景
-        let bg = Background.new(id['bg.png'])
-        bg.name = 'bg'
-        this.addSprite(bg)
-        //血条
-        let phpin = Hp.new(10, 10, 50, 5)
-        phpin.name = 'phpin'
-        let phpout = Hp.new(10, 10, 50, 5, 'red')
-        phpout.name = 'phpout'
-        this.addRect(phpin)
-        this.addRect(phpout)
-        //玩家
-        let wq = Player.new(id['wq.png'])
-        wq.name = 'wq'
-        wq.setSpeed(5, 5)
-        wq.setPosition(32, canvas.height / 2 - wq.height / 2)
-        this.addSprite(wq)
-        //武器
-        let mp5 = Player.new(id['mp5.png'])
-        mp5.name = 'mp5'
-        mp5.rotate = 10
-        this.addSprite(mp5)
-        //子弹
-        let bullets = new Bullet(g)
-        let bullet = Player.new(id['bullet.png'])
-        bullet.name = 'bullet'
-        bullet.setSpeed(7, 7)
-        bullet.setPosition(wq.x + wq.width + 10, wq.y + wq.height / 2 + 15)
-        this.addList(bullets)
-        //怪物
-        let numOfBlobs = 6, //数量
-            spacing = 48, //间隔
-            xOffset = 150, //x坐标
-            speed = .3, // 速度
-            direction = 1 // 运动方向
-        let monster = new Monster(g)
-        monster.name = 'monster'
-        for (let i = 0; i < numOfBlobs; i++) {
-            let yt = Player.new(id['yt.png'])
-            yt.name = 'yt' + i
-            let x = spacing * i + xOffset
-            let y = randomNum(32, canvas.height - yt.height - 32)
-            yt.setSpeed(speed * direction, speed * direction)
-            yt.setPosition(x, y)
-            if (randomNum(0, 100) % 2 === 0) {
-                direction *= 1
-            } else {
-                direction *= -1
-            }
-            monster.blobs.push(yt)
-        }
-        this.addList(monster)
-    }
-
-    addSprite(obj) {
-        this.sprites[obj.name] = obj
-    }
-
-    addList(obj) {
-        this.list[obj.name] = obj
-    }
-
-    addRect(obj) {
-        this.rects[obj.name] = obj
     }
 }
--- public/10.29/images/texture.js
+++ public/10.29/images/texture.js
@@ -11,15 +11,31 @@ const texture = {
             },
         "bullet.png":
             {
-                "frame": {"x": 516, "y": 113, "w": 5, "h": 5},
+                "frame": {"x": 533, "y": 175, "w": 10, "h": 5},
                 "rotated": false,
                 "trimmed": false,
-                "spriteSourceSize": {"x": 0, "y": 0, "w": 5, "h": 5},
-                "sourceSize": {"w": 5, "h": 5}
+                "spriteSourceSize": {"x": 0, "y": 0, "w": 10, "h": 5},
+                "sourceSize": {"w": 10, "h": 5}
+            },
+        "bullet2.png":
+            {
+                "frame": {"x": 516, "y": 175, "w": 15, "h": 15},
+                "rotated": false,
+                "trimmed": false,
+                "spriteSourceSize": {"x": 0, "y": 0, "w": 15, "h": 15},
+                "sourceSize": {"w": 15, "h": 15}
             },
         "mp5.png":
             {
-                "frame": {"x": 516, "y": 101, "w": 30, "h": 10},
+                "frame": {"x": 516, "y": 151, "w": 30, "h": 10},
+                "rotated": false,
+                "trimmed": false,
+                "spriteSourceSize": {"x": 0, "y": 0, "w": 30, "h": 10},
+                "sourceSize": {"w": 30, "h": 10}
+            },
+        "mp5_l.png":
+            {
+                "frame": {"x": 516, "y": 163, "w": 30, "h": 10},
                 "rotated": false,
                 "trimmed": false,
                 "spriteSourceSize": {"x": 0, "y": 0, "w": 30, "h": 10},
@@ -31,6 +47,14 @@ const texture = {
                 "rotated": false,
                 "trimmed": true,
                 "spriteSourceSize": {"x": 8, "y": 0, "w": 36, "h": 48},
+                "sourceSize": {"w": 48, "h": 48}
+            },
+        "wq_l.png":
+            {
+                "frame": {"x": 516, "y": 101, "w": 36, "h": 48},
+                "rotated": false,
+                "trimmed": true,
+                "spriteSourceSize": {"x": 4, "y": 0, "w": 36, "h": 48},
                 "sourceSize": {"w": 48, "h": 48}
             },
         "yt.png":
@@ -45,10 +69,10 @@ const texture = {
     "meta": {
         "app": "http://www.codeandweb.com/texturepacker",
         "version": "1.0",
-        "image": "police.png",
+        "image": "texture.png",
         "format": "RGBA8888",
         "size": {"w": 557, "h": 515},
         "scale": "1",
-        "smartupdate": "$TexturePacker:SmartUpdate:92c3f4acff2732ac223423cd92546608:e859f19c566083731962f3bab5e2097e:da7918761e6bbec3d2b85b0668dbb547$"
+        "smartupdate": "$TexturePacker:SmartUpdate:cdc8ba58fc0c16218ad0a0ff9fab8757:317176c5263cddf8381acef211ee3b85:1324de366594c49aba2332ca8e5048d3$"
     }
 }
